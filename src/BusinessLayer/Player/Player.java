08dfdb985485abed10023bd69541991cd0815f35
EmirkanGuz <emirkanguz@gmail.com>
2025-03-19 21:12:05 +0300

    asd

--- src/BusinessLayer/Player/Player.java
+++ src/BusinessLayer/Player/Player.java
@@ -1,4 +1,18 @@
 package src.BusinessLayer.Player;
 
 public class Player {
+    private String name;
+    private ArrayList<Card> hand;
+}
+
+public Card playCard() {
+    // TODO
+}
+
+public int getNumberOfCards() {
+    return hand.length();
+}
+
+public ArrayList<Card> pickCard(Pile pile, int amount) {
+    // TODO
 }
