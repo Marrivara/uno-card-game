23f6b694337a24644c62e2418756cd7c2b406ea1
Berkay <berkaydulgerbaki@gmail.com>
2025-04-21 20:39:40 +0300

    dsadsd

--- UnoCardGame.java
+++
@@ -1,9 +0,0 @@
-import src.BusinessLayer.Referee.Referee;
-
-public class UnoCardGame {
-
-    public static void main(String[] args) {
-        Referee ref = new Referee();
-        ref.playGame();
-    }
-}
